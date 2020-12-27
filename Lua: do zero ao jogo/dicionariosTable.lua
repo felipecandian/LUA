5cd43a2da54b13c02c92c06a0d782d13c0ff4e20
Felipe Candian <felipecandian95@gmail.com>
2020-12-27 01:16:14 +0000

    Atualização dos table

---
+++ Lua: do zero ao jogo/dicionariosTable.lua
@@ -0,0 +1,11 @@
+copas = {
+  [58] = "Suecia",
+  [62] = "Chile",
+  [70] = "Mexico",
+  [94] = "EUA",
+  [02] = "Japão e Coreira do Sul",
+}
+
+for chave, valor in pairs(copas) do
+  print(chave,valor)
+end
