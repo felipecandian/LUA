fa8e38148a5034f304c1aae5ad1fa92cc39670a3
Felipe Candian <felipecandian95@gmail.com>
2020-12-27 20:22:13 +0000

    Arrumando os bugs

--- main.lua
+++ main.lua
@@ -1,91 +1,85 @@
 LARGURA_TELA = 320
 ALTURA_TELA = 480
-MAX_METEOROS = 10
+MAX_METEOROS = 12
 
 aviao_14bis = {
-  src = "imagens/14bis.png",
-  largura = 64,
-  altura = 64,
-  x = LARGURA_TELA/2 -64/2,
-  y = ALTURA_TELA - 64/2
+    src = "imagens/14bis.png",
+    largura = 64,
+    altura = 64,
+    x = LARGURA_TELA/2 - 64/2,
+    y = ALTURA_TELA - 64/2
 }
 
 meteoros = {}
 
 function removeMeteoros()
-  for i = #meteoros, 1, -1 do
-    if meteoros[i].y > ALTURA_TELA then
-      table.remove(meteoros, i)
+    for i = #meteoros, 1, -1 do
+        if meteoros[i].y > ALTURA_TELA then
+            table.remove(meteoros, i)
+        end
     end
-   end 
 end
 
 function criaMeteoro()
-  meteoro = {
-    x = math.random(LARGURA_TELA),
-    y = -70,
-    peso = math.random(3),
-    deslocamento_horizontal = math.random(-1,1)
-  }
-  table.insert(meteoros, meteoro)
+    meteoro = {
+        x = math.random(LARGURA_TELA),
+        y = -70,
+        peso = math.random(3),
+        deslocamento_horizontal = math.random(-1, 1)
+    }
+    table.insert(meteoros, meteoro)
 end
 
 function moveMeteoros()
-  for k, v in pairs(meteoros) do
-    meteoro.y = meteoro.y + meteoro.peso
-    meteoro.x = meteoro.x + meteoro.deslocamento_horizontal
-  end
+    for k, meteoro in pairs(meteoros) do
+        meteoro.y = meteoro.y + meteoro.peso
+        meteoro.x = meteoro.x + meteoro.deslocamento_horizontal
+    end
 end
 
 function move14bis()
-  if love.keyboard.isDown('w') then
-      aviao_14bis.y = aviao_14bis.y-1
+    if love.keyboard.isDown('w') then
+        aviao_14bis.y = aviao_14bis.y - 1
     end
     if love.keyboard.isDown('s') then
-      aviao_14bis.y = aviao_14bis.y+1
+        aviao_14bis.y = aviao_14bis.y + 1
     end
     if love.keyboard.isDown('a') then
-      aviao_14bis.x = aviao_14bis.x-1
+        aviao_14bis.x = aviao_14bis.x - 1
     end
     if love.keyboard.isDown('d') then
-      aviao_14bis.x = aviao_14bis.x+1
+        aviao_14bis.x = aviao_14bis.x + 1
     end
-end
-
+end    
 
 function love.load()
-  love.window.setMode(LARGURA_TELA, ALTURA_TELA, {resizable = false})
-  love.window.setTitle("14bis vs Meteoros")
+    love.window.setMode(LARGURA_TELA, ALTURA_TELA, {resizable = false})
+    love.window.setTitle("14bis vs Meteoros")
 
-  math.randomseed(os.time()) 
+    math.randomseed(os.time())
 
-  background = love.graphics.newImage("imagens/background.png")
-
-  aviao_14bis.imagem = love.graphics.newImage(aviao_14bis.src)
-   
-   meteoro_img = love.graphics.newImage("imagens/meteoro.png")
+    background = love.graphics.newImage("imagens/background.png")
+    aviao_14bis.imagem = love.graphics.newImage(aviao_14bis.src)
+    meteoro_img = love.graphics.newImage("imagens/meteoro.png")
 end
- 
--- Increase the size of the rectangle every frame.
+
 function love.update(dt)
-    if love.keyboard.isDown('w','a','s','d') then
-      move14bis()
+    if love.keyboard.isDown('w', 'a', 's', 'd') then
+        move14bis()
     end
-   
-   if #meteoros < MAX_METEOROS then
-    criaMeteoro()
-   end 
-   
+
+    removeMeteoros()
+    if #meteoros < MAX_METEOROS then
+        criaMeteoro()
+    end
     moveMeteoros()
 end
 
 function love.draw()
-
-    love.graphics.draw(background, 0,0)
-
-    love.graphics.draw(aviao_14bis.imagem,aviao_14bis.x, aviao_14bis.y)
-
-    for k,meteoro in pairs(meteoros) do
-      love.graphics.draw(meteoro_img, meteoro.x, meteoro.y)
+    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(aviao_14bis.imagem, aviao_14bis.x, aviao_14bis.y)
+    
+    for k, meteoro in pairs(meteoros) do
+        love.graphics.draw(meteoro_img, meteoro.x, meteoro.y) 
     end
 end
